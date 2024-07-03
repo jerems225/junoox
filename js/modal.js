1222d32b469f0421fd4ae31fa88b8c16d98591a5
Jeremy Niondo <jeremieniondo@gmail.com>
2024-07-03 17:21:47 +0000

    add product card

---
+++ js/modal.js
@@ -0,0 +1,15 @@
+var modal = document.querySelector("#myModal");
+var modalContent = document.querySelector("#modal-content");
+
+var btn = document.querySelector(".menu-icon");
+
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal || event.target === modalContent) {
+        modal.style.display = "none";
+    }
+}
