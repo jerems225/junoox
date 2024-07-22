f990dc25d187d608a6042727d899fd3adabcbf60
Jeremy Niondo <jeremieniondo@gmail.com>
2024-07-22 15:50:03 +0000

    responsive header and about page

--- resources/js/main.js
+++ resources/js/main.js
@@ -18,10 +18,19 @@
 
 const menuIcon = document.querySelector('#menu_dropdown_icon');
 const navServicesLinks = document.querySelector('.navbar_services_links');
-// menuIcon ? 
-// menuIcon.onclick  = () => {
-//   console.log(navServicesLinks.classList.toggle('active'))
-// }
+
+menuIcon.addEventListener('click', () => {
+  const ActiveNav = navServicesLinks.classList.toggle('active');
+  if(ActiveNav){
+    menuIcon.children[0].style.display = 'none';
+    menuIcon.children[1].style.display = 'flex';
+  }
+  else {
+    menuIcon.children[0].style.display = 'flex';
+    menuIcon.children[1].style.display = 'none';
+  }
+})
+
 
 
 
